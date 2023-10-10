a50596f6733cea9d7d12f90d0764232011f8ca11
FNH Rasthol <founoukossiherve@gmail.com>
2023-10-10 14:14:33 +0200

    My Second Commit

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,36 @@
+/*--------------------------------------Importing------------------------------------------------------------ */
+
+// Import next Image
+import Image from "next/image";
+// Import next Link
+import Link from "next/link";
+// Import Components
+import socials from "../components/Socials";
+import Socials from "../components/Socials";
+
+/*---------------------------------------------------Construction-------------------------------------------- */
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 lx:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8 ">
+          {/* Logo */}
+          <Link href={"/"}>
+            <Image
+              src={"/logo.svg"}
+              width={220}
+              height={48}
+              alt=""
+              priority={true}
+            />
+          </Link>
+          {/* Social */}
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
